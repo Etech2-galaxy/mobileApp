58e0464b069af6a59787c940dc0d601bde0ffb2f
rnfor-pro <nfor.rudolph1@gmail.com>
2025-03-29 12:24:26 -0400

    logic added

---
+++ module1/addition.sh
@@ -0,0 +1,12 @@
+#script to analysed special variables
+#!/bin/bash
+
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Second Parameter : $3"
+echo "Second Parameter : $4"
+echo "Second Parameter : $5"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#"
